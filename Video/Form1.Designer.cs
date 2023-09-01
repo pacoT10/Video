814d027bc578a292abff62d8f7ef3735aca3fe87
root <mgiraudo@ies21.edu.ar>
2023-09-01 12:43:20 -0300

    Agregar archivos de proyecto.

---
+++ Video/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.wmp = new AxWMPLib.AxWindowsMediaPlayer();
+            this.cboCancion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wmp
+            // 
+            this.wmp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wmp.Enabled = true;
+            this.wmp.Location = new System.Drawing.Point(12, 57);
+            this.wmp.Name = "wmp";
+            this.wmp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp.OcxState")));
+            this.wmp.Size = new System.Drawing.Size(486, 237);
+            this.wmp.TabIndex = 0;
+            this.wmp.Enter += new System.EventHandler(this.wmp_Enter);
+            // 
+            // cboCancion
+            // 
+            this.cboCancion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboCancion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCancion.FormattingEnabled = true;
+            this.cboCancion.Location = new System.Drawing.Point(12, 30);
+            this.cboCancion.Name = "cboCancion";
+            this.cboCancion.Size = new System.Drawing.Size(486, 32);
+            this.cboCancion.TabIndex = 1;
+            this.cboCancion.SelectedIndexChanged += new System.EventHandler(this.cboCancion_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccione su cancion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 298);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboCancion);
+            this.Controls.Add(this.wmp);
+            this.Name = "Form1";
+            this.Text = "Mis canciones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmp;
+        private System.Windows.Forms.ComboBox cboCancion;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Video/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Video
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void wmp_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wmp.uiMode = "none";
+
+            StreamReader sr = new StreamReader("lista.txt");
+
+            string linea;
+
+            while(sr.EndOfStream==false)
+            {
+                linea = sr.ReadLine();
+                string[] datos = linea.Split(',') ;
+                cboCancion.Items.Add(datos[1]);
+
+            }
+
+            cboCancion.SelectedIndex = 0;
+            sr.Close();
+            sr.Dispose();
+        }
+
+        private void cboCancion_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+           string cancion=  "tema"+ (cboCancion.SelectedIndex+1).ToString()+".mp4";
+            wmp.URL = cancion;
+        }
+    }
+}
